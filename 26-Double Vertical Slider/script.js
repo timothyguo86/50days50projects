11adee301c8ed8e3eab79a9050a3bd598f327036
Timothy Guo <timothyguo86@gmail.com>
2024-01-22 15:14:03 -0500

    (feat) 26-Double Vertical Slider

---
+++ 26-Double Vertical Slider/script.js
@@ -0,0 +1,31 @@
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+
+const slidesLength = slideLeft.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = direction => {
+  if (direction === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0
+    }
+  } else {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * 100}vh)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * 100}vh)`
+  }
+}
