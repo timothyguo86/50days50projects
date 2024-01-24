ed053aa51ed4a21ce1750c918e13c93a57000840
Timothy Guo <timothyguo86@gmail.com>
2024-01-24 02:04:45 -0500

    (feat) 48-Random Image Feed

---
+++ 48-Random Image Feed/script.js
@@ -0,0 +1,18 @@
+const IMG_URL = 'https://source.unsplash.com/random/'
+const container = document.querySelector('.container')
+const rows = 10
+
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement('img')
+
+  img.src = `${IMG_URL}${getRandomSize()}`
+  container.append(img)
+}
+
+function getRandomSize() {
+  return `${getRandomNum()}x${getRandomNum()}`
+}
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 20) + 300
+}
